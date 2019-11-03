cc8c3c5c2d2f457341f71fd2bffc9c01c2a99f32
kingofmidas <kingofmidas@protonmail.com>
2019-11-03 17:54:52 +0300

    write docstrings. Update convertion function

--- rss/convert.py
+++ rss/convert.py
@@ -6,62 +6,100 @@ from bs4 import BeautifulSoup
 import dominate
 from dominate.tags import *
 import pdfkit
-import webbrowser
+import webbrowser  
 
 
-def getDescription(item):
-    return BeautifulSoup(item, features="html.parser").getText()
-
-
-def checkMediaContent(item):
-    media_content = '\n'
-    if(item.has_key('media_content')):
-        media_content = item.media_content[0]['url']
-    return media_content    
-
-
+# make convertion of news into pdf or html
 def makeConvertion(url, html_path, pdf_path, limit):
-
+    '''
+    1. create object of feedparser library
+    2. create object of dominate library for creating html files
+    3. on each iteration 
+        3.1 check if there is media content
+        3.2 write html tags
+        3.3 convert format of file
+    '''
     channel = feedparser.parse(url)
     doc = dominate.document(title="HTML document")
 
-    index = 0
+    for index, item in enumerate(channel.entries):
+        if(index == limit):
+            break
 
-    while (index < limit):
-        item = channel.entries[index]
-
-        media = checkMediaContent(item)
-        
-        rand = random.randint(1, 120)
-        if not os.path.exists('images/'):
-            os.makedirs('images/')
-        filename_ = 'images/' + str(rand) + '.jpg'
-        urllib.request.urlretrieve(media, filename_)
+        random_name = random.randint(1, 120)
+        media_file_name = checkMediaContent(item, random_name)
 
         with doc:
             with div():
                 h2(item.title)
                 p("Date: " + item.published)
-                img(src=os.path.abspath(filename_))
+                if (media_file_name):
+                    img(src=os.path.abspath(media_file_name))
                 p("Description: " + getDescription(item.description))
-        index += 1
 
+    
     if(html_path):
-        with open('test.css') as file:
-            css = file.read()
-
-        with doc.head:
-            style(css)
-
-        if not os.path.exists(html_path):
-            os.makedirs(html_path)
-        html_file = html_path + str(rand) + '.html'
-        with open(html_file, 'w') as f:
-            f.write(str(doc))
-        webbrowser.open(html_file, new=2)
+        intoHTML(html_path, doc, random_name)
 
     elif(pdf_path):
-        if not os.path.exists(pdf_path):
+        intoPDF(pdf_path, doc, random_name)
+
+
+# return description of news without html tags
+def getDescription(item):
+    return BeautifulSoup(item, features="html.parser").getText()
+
+
+def checkMediaContent(item, random_name):
+    '''
+    1. create folder with images and return name of image 
+    if there is media content in news
+    2. or return nothing
+    '''
+    media_content = ''
+    if('media_content' in item.keys()):
+        media_content = item.media_content[0]['url']
+    
+        if not os.path.exists('images/'):
+            os.makedirs('images/')
+        filename_ = 'images/' + str(random_name) + '.jpg'
+        urllib.request.urlretrieve(media, filename_)
+        return filename_
+
+    return media_content  
+
+
+def intoHTML(html_path, doc, random_name):
+    '''
+    1. read file with styles for html
+    2. write styles in html
+    3. create folder for html file if not exists
+    4. write html file
+    5. open browser with html
+    '''
+    with open('style.css') as file:
+            css_file = file.read()
+
+    with doc.head:
+        style(css_file)
+
+    if not os.path.exists(html_path):
+        os.makedirs(html_path)
+    html_file = html_path + str(random_name) + '.html'
+
+    with open(html_file, 'w') as f:
+        f.write(str(doc))
+
+    webbrowser.open(html_file, new=2)
+
+
+def intoPDF(pdf_path, doc, random_name):
+    '''
+    1. create folder for pdf files if not exists
+    2. write pdf file
+    '''
+    if not os.path.exists(pdf_path):
             os.makedirs(pdf_path)
-        pdf_file = pdf_path + str(rand) + '.pdf'
-        pdfkit.from_string(str(doc), pdf_file)
+    pdf_file = pdf_path + str(random_name) + '.pdf'
+
+    pdfkit.from_string(str(doc), pdf_file)
